b1cbf87cd7e4a89f7a50d4601c23154a7e4c1495
Julian Rosse <julian@helixbass.net>
2023-08-16 08:47:34 -0400

    test cases

--- tests/target/key-value-macro.rs
+++ tests/target/key-value-macro.rs
@@ -1,3 +1,19 @@
 rule! {
     key => value,
 }
+
+// multiple keys and values
+rule! {
+    key => value,
+    key2 => value2,
+}
+
+// weird spacing
+rule! {
+    key => value,
+}
+
+// no trailing comma
+rule! {
+    key => value,
+}
